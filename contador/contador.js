e0ddb4a3fbe55c42ef6f60bb38aa9606acdfb746
hacktall <jmariajosebueno@gmail.com>
2025-04-08 20:46:33 -0300

    codigos adicionados

---
+++ contador/contador.js
@@ -0,0 +1,27 @@
+const decreasebutton=document.getElementById("decrease");
+const increasebutton=document.getElementById("increase");
+const resetbutton=document.getElementById("reset");
+const countbt=document.getElementById("contador");
+let count=0;
+
+
+increasebutton.onclick=function(){
+ count++;
+ countbt.textContent=count;
+
+}
+decreasebutton.onclick=function(){
+ cout--;
+ countbt.textContent=count;
+
+}
+resetbutton.onclick=function(){
+ count=0;
+ countbt.textContent=count;
+}
+
+
+
+
+
+
